ba0d0d3c81282f4613853fdeac9e357f5954dad1
Cristobal Sanchez <cristobal.sanchez@globant.com>
2019-11-25 19:57:55 -0300

    Modify README.md. Add spacy examples.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * https://likegeeks.com/es/tutorial-de-nlp-con-python-nltk/
 * https://www.aprendemachinelearning.com/ejercicio-nlp-cuentos-de-hernan-casciari-python-espanol/
 * https://relopezbriega.github.io/blog/2017/09/23/procesamiento-del-lenguaje-natural-con-python/
+* https://medium.com/@ageitgey/natural-language-processing-is-fun-9a0bff37854e
---
+++ spacy_extract_facts.py
@@ -0,0 +1,19 @@
+import spacy
+import textacy.extract
+
+nlp = spacy.load('en_core_web_sm')
+
+text = """London is the capital and most populous city of England and the United Kingdom.  
+Standing on the River Thames in the south east of the island of Great Britain, 
+London has been a major settlement for two millennia. It was founded by the Romans, 
+who named it Londinium.
+"""
+doc = nlp(text)
+
+statements = textacy.extract.semistructured_statements(doc, "London")
+
+print("Here are the things I know about London:")
+
+for statement in statements:
+    subject, verb, fact = statement
+    print(f" - {fact}")
